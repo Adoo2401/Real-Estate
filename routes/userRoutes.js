81f8d5e69afd3b2005e627c0eb7092e141691143
adoo2401 <abdullahrehmani230@gmail.com>
2022-07-30 17:58:48 +0500

    controllers

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { RegisterUser, loginUser,addFavourite, getFavourites, delFavourite, adminGetUsers, adminEdit, adminDelete, adminGetSingleUser, recent, getRecent, deleteNotification } = require("../controllers/userController");
+const { RegisterUser, loginUser,addFavourite, getFavourites, delFavourite, adminGetUsers, adminEdit, adminDelete, adminGetSingleUser, recent, getRecent, deleteNotification, getFCMToken } = require("../controllers/userController");
 const verifyRole = require("../middlewares/verifyRole");
 const verifyToken = require("../middlewares/verifyToken");
 const router = express.Router();
@@ -14,6 +14,7 @@ router.route('/del_fav/:property_id').delete(verifyToken,delFavourite);
 router.route("/recents/:propertyId").post(verifyToken,recent);
 router.route("/getRecent").get(verifyToken,getRecent);
 router.route("/deleteNotification/:notificationId").delete(verifyToken,deleteNotification)
+router.route("/getToken").post(verifyToken,getFCMToken)
 
 //admin routes
 
